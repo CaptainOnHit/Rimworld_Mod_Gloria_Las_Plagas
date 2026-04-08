5a86e7115c40bb6551b95607ed09fb9d72aacf29
Tom K <tom.kindermann@stud.fra-uas.de>
2026-04-08 21:34:07 +0200

    added a check so infected pawns cant get another parasite

---
+++ Gloria Las Plagas/Source/GloriaLasPlagas/GloriaLasPlagas/Recipe_AddHediff_PlagaCheck.cs
@@ -0,0 +1,41 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace CCDevelopment.LasPlagas
+{
+    public class Recipe_AddHediff_PlagaCheck : Recipe_AddHediff
+    {
+        protected List<String> filteredXenotypes = new List<String>();
+        
+        public Recipe_AddHediff_PlagaCheck() 
+        { 
+            fillFilteredXenotypes();
+        }
+        
+        public override bool AvailableOnNow(Thing thing, BodyPartRecord part = null)
+        {
+            if (!base.AvailableOnNow(thing, part))return false;
+            Pawn pawn = thing as Pawn;
+            if (pawn == null) return false;
+            String xenotypeName = pawn.genes?.Xenotype?.defName;
+            foreach(String xenotype in filteredXenotypes)
+            {
+                if(xenotype == xenotypeName) return false;
+            }
+            return true;
+        }
+
+        public void fillFilteredXenotypes()
+        {
+            filteredXenotypes.Add("CCDevelopment_LasPlagas_Ganado");
+            filteredXenotypes.Add("CCDevelopment_LasPlagas_Guadana");
+            filteredXenotypes.Add("CCDevelopment_LasPlagas_Mandibula");
+            filteredXenotypes.Add("CCDevelopment_LasPlagas_Superior");
+        }
+    }
+}
